c6eaaea9c6748a3ddd8b9e35e17e015bb48ba50a
panda <panda@DESKTOP-FJUADB3>
2025-04-22 22:22:54 +0500

    Добавьте файлы проекта.

---
+++ hwCSharpp/Money.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwCSharpp
+{
+    public class Money
+    {
+        public double Sum { get; set; }
+
+        public static implicit operator Money(double sum)
+        {
+            return new Money(sum);
+        }
+        public static Money operator+(Money num, Money num1)
+        {
+            return new Money(num.Sum + num1.Sum);
+        }
+        public static Money operator+(Money num, double num1)
+        {
+            Money res = num + num1;
+            return res;
+        }
+        public static Money operator-(Money num, Money num1)
+        {
+            return new Money(num1.Sum - num1.Sum);
+        }
+        public static Money operator-(Money num, double num1)
+        {
+            Money res = num1 - num1;
+            return res;
+        }
+        public static Money operator*(Money num, int num1)
+        {
+            return new Money(num.Sum * num1);
+        }
+        public static Money operator/(Money num, int num1)
+        {
+            if (num1 == 0)
+            {
+                throw new DivideByZeroException("Делить на 0 нельзя");
+            }
+            return new Money(num.Sum / num1);
+        }
+        public static Money operator++(Money num)
+        {
+            return new Money(num.Sum + 0.1);
+        }
+        public static Money operator--(Money num)
+        {
+            return new Money(num.Sum - 0.1);
+        }
+        public static bool operator>(Money num, Money num1)
+        {
+            return num.Sum > num1.Sum;
+        }
+        public static bool operator<(Money num, Money num1)
+        {
+            return num.Sum < num1.Sum;
+        }
+        public static bool operator==(Money num, Money num1)
+        {
+            return num.Sum == num1.Sum;
+        }
+        public static bool operator!=(Money num, Money num1)
+        {
+            return num.Sum != num1.Sum;
+        }
+        public Money(double sum)
+        {
+            Sum = sum;
+        }
+    }
+}
---
+++ hwCSharpp/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace hwCSharpp
+{
+    public class Bank: Exception
+    {
+        public Bank(string message) : base(message) { }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money num1 = 257;
+            Money num2 = 600;
+
+
+            try
+            {
+                CheckResult(num1 + num2, "+");
+                CheckResult(num2 - num1, "-");
+                CheckResult(num1 * 2, "*");
+                CheckResult(num1 / 2, "/");
+                CheckResult(num1++, "num1++");
+                CheckResult(num2++, "num2++");
+                CheckResult(num1--, "num1--");
+                CheckResult(num2--, "num2--");
+                Console.WriteLine($"==: {num1 == num2}");
+                Console.WriteLine($"!=: {num1 != num2}");
+                Console.WriteLine($">: {num1 > num2}");
+                Console.WriteLine($"<: {num1 < num2}");
+            }
+            catch (Bank ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void CheckResult(Money result, string operation)
+        {
+            if (result.Sum < 0)
+            {
+                throw new Bank($"Нищий, итог операции {operation} меньше 0: {result.Sum}");
+            }
+            Console.WriteLine($"{operation}: {result.Sum}");
+        }
+    }
+}
